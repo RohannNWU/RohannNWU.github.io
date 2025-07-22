2d9baac6e68df0e34b774138a3f5bb32e077bb9d
RohannNWU <25130757@mynwu.ac.za>
2025-07-22 15:44:29 +0200

    Added NavBar

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-function handleSelection() {
-    const dropdown = document.getElementById("myDropdown");
-    const selectedValue = dropdown.ariaValueMax;
+document.addEventListener("DOMContentLoaded", function() {
+    const dropdown = document.getElementById("menuDropdown");
     const result = document.getElementById("result");
 
-    if (selectedValue) {
-        result.textContent = "You selected: " + selectedValue;
-    } else {
-        result.testContent = "";
-    }
-}
+    dropdown.addEventListener("change", function() {
+        const selected = dropdown.value;
+
+        if (selected) {
+            result.textContent = "You selected: " + selected.charAt(0).toUpperCase() + selected.slice(1);
+        } else {
+            result.textContent = "Please select an option from the menu."
+        }
+    });
+});
