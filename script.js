26ee0a4b9b793dc91b89c81a15679d57525a6640
RohannNWU <25130757@mynwu.ac.za>
2025-07-24 10:01:30 +0200

    Added more flesh to the website

--- script.js
+++ script.js
@@ -3,18 +3,28 @@ const navLinks = document.querySelectorAll("nav a");
 
 function loadAboutMe() {
     dynamicContent.innerHTML = `
-        <img src="images/headshot.png" alt="Rohann Venter">
+        <img src="images/headshot.png" alt="Rohann Venter" style="box-shadow: 0 0 10px rgba(0,0,0,0.3);">
         <p></p>
         <h2>About Me</h2>
-        <p>Hello World! My name is Rohann Venter, and I currently work as a Route Checker In-Charge Flight Attendant while pursuing my BSc in IT as a 3rd-year student.
-        I’ve developed a diverse skill set over the years, including advanced proficiency in Microsoft Office, time management, project management, communication, and C# programming.
-        I also have intermediate expertise in Java and am building my knowledge of SQL databases.</p>
+        <p>Hello World! My name is Rohann Venter.</p>
 
-        <p>Throughout my career, I’ve achieved significant milestones, including my promotion to In-Charge in February 2019 and securing the Route Checker position in February 2024.
-        These experiences have honed my leadership and analytical skills, preparing me for my next goal—transitioning into the software development industry.</p>
+        <p>I currently reside in the Northern Suburbs of Cape Town.</p>
+        
+        <p>I am a full-time In-Charge Route Check Flight Attendant at South Africa's biggest low-cost airline, FlySafair.
+        I have achieved significant milestones throughout my 8 year career, including a promotion to In-Charge Flight Attendant in February 2019 and achieving the
+        Route Checker position in February 2024. I have almost 6000 flight hours on the Boeing 737, and I have had the opportunity to be witness of quite a handful 
+        of experiences over the years. These experiences have helped me hone my leadership, especially in critically pressured environments, and analytical skills, 
+        preparing me for my next goal — transitioning into the software development industry.</p>
 
-        <p>I’m passionate about specializing in C#, ASP.NET, and SQL, or further advancing my skills in Java. Ultimately, I aim to become either a lead developer or a consulting developer,
-        contributing to innovative projects that drive meaningful impact. I’m excited about the journey ahead and look forward to connecting with others who share similar goals and interests.</p>
+        <p>I am currently a 3rd year BSc IT student at the North-West University. I have developed a great love for programming over the years and I have realised 
+        that I am at my happiest behind the laptop developing little programs.</p>
+        </br>
+        <p>
+            <a href="files/RVenter_Curriculum_Vitae.pdf" target="_blank">
+                <img src="images/pdf.png" style="max-width: 20px;">
+                Download my Curriculum Vitae
+            </a>
+        </p>
     `;
     setActiveLink(document.getElementById("about-link"));
 }
@@ -22,10 +32,26 @@ function loadAboutMe() {
 function loadExperience() {
     dynamicContent.innerHTML = `
         <h2>Experience</h2>
-        <h3>In-Charge Route Check Flight Attendant</h3>
-        <p>Safair Operations Pty (Ltd) t/a FlySafair&emsp;June 2017 - Present</p>
-        <p></p>
-        <p></p>
+        <div class="job-entry">
+            <h3>In-Charge Route Check Flight Attendant</h3>
+            <h4>Safair Operations Pty (Ltd) t/a FlySafair&nbsp;&nbsp;|&nbsp;&nbsp;June 2017 - Present</h4>
+            <p>
+                <ul class="aircraft">
+                    <li>Promoted to In-Charge Flight Attendant in February 2019. Achieved Route Checker rank in February 2024.</li>
+                    <li>Overall responsibility for conduct and coordination of normal and emergency cabin procedures</li>
+                    <li>Conduct in-flight proficiency checks on company employed flight attendants</li>
+                </ul
+            </p>
+            <h3>IT Assistant</h3>
+            <h4>Hottentots-Holland High School&nbsp;&nbsp;|&nbsp;&nbsp;February 2017 - June 2017</h4>
+            <p>
+                <ul class="computer">
+                    <li>Assisted with day-to-day workstation issues</li>
+                    <li>Escalated serious issues to the service provider</li>
+                    <li>Built PCs and added workstations to the network</li>
+                </ul>
+            </p>
+        </div>
     `;
     setActiveLink(document.getElementById("experience-link"));
 }
@@ -33,7 +59,27 @@ function loadExperience() {
 function loadSkills() {
     dynamicContent.innerHTML = `
         <h2>Skills</h2>
-        <p></p>
+        <div class="job-entry">
+            <p>
+                <h3>General Skills</h3>
+                <ul class="general">
+                    <li>Leadership</li>
+                    <li>Communication</li>
+                    <li>Teamwork</li>
+                    <li>Time Management</li>
+                    <li>Project Management</li>
+                </ul>
+            </p>
+            <p>
+                <h3>Programming Skills</h3>
+                <ul class="coding">
+                    <li>C# with ASP.NET</li>
+                    <li>SQL in Oracle</li>
+                    <li>Python</li>
+                    <li>Java</li>
+                </ul>
+            </p>
+        </div>
     `;
     setActiveLink(document.getElementById("skills-link"));
 }
@@ -41,27 +87,104 @@ function loadSkills() {
 function loadEducation() {
     dynamicContent.innerHTML = `
         <h2>Education</h2>
-        <p></p>
+        <div class="job-entry">
+            <h3>North-West University</h3>
+            <h4>Bachelor of Science in Information Technology</h4>
+            <h4>Graduation estimated 2025</h4>
+
+            <h3>Cranfield Aviation Training</h3>
+            <h4>Train-the-Trainer Course</h4>
+            <h4>Completed November 2023</h4>
+
+            <h3>Damelin</h3>
+            <h4>Short Course in Project Management</h4>
+            <h4>Completed in 2020</h4>
+            
+            <h3>Future Flyers Training Academy</h3>
+            <h4>SACAA Cabin Crew License</h4>
+            <h4>Completed October 2016</h4>
+
+            <h3>Richards Bay High School</h3>
+            <h4>Matriculated 2013</h4>
+            <table class="marks-table">
+                <tr>
+                    <td>Afrikaans Home Language</td>
+                    <td>72%</td>
+                </tr>
+                <tr>
+                    <td>English First Additional Language</td>
+                    <td>76%</td>
+                </tr>
+                <tr>
+                    <td>Mathematics</td>
+                    <td>80%</td>
+                </tr>
+                <tr>
+                    <td>Life Orientation</td>
+                    <td>68%</td>
+                </tr>
+                <tr>
+                    <td>Engineering Graphics and Design</td>
+                    <td>82%</td>
+                </tr>
+                <tr>
+                    <td>Hospitality Studies</td>
+                    <td>90%</td>
+                </tr>
+                <tr>
+                    <td>Physical Sciences</td>
+                    <td>58%</td>
+                </tr>
+                <tr>
+                    <td>Mathematics Paper 3</td>
+                    <td>47%</td>
+                </tr>
+            </table>
+        </div>
     `;
     setActiveLink(document.getElementById("education-link"));
 }
 
+function loadProject() {
+    dynamicContent.innerHTML = `
+        <h2>My Projects</h2>
+        <p></p>
+    `;
+    setActiveLink(document.getElementById("project-link"));
+}
+
 function loadContact() {
     dynamicContent.innerHTML = `
-        <h2>Contact</h2>
-        <p>
-            <a href="https://www.linkedin.com/in/rohann-venter-67b39a136/" target="_blank">
-                <img src="images/linkedin.png" style="max-width: 20px;">
-                Rohann Venter
-                </a>
-        </p>
+        <h2>Contact Me</h2>
+        <div style="justify-items: left;">
+            <p>
+                <a href="https://www.linkedin.com/in/rohann-venter-67b39a136/" target="_blank">
+                    <img src="images/linkedin.png" style="max-width: 20px;">
+                    LinkedIn Profile - Rohann Venter
+                    </a>
+            </p>
 
-        <p>
-            <a href="https://github.com/RohannNWU" target="_blank">
-                <img src="images/github.png" style="max-width: 20px;">
-                RohannNWU
-            </a>
-        </p>
+            <p>
+                <a href="https://github.com/RohannNWU" target="_blank">
+                    <img src="images/github.png" style="max-width: 20px;">
+                    GitHub Page - RohannNWU
+                </a>
+            </p>
+
+            <p>
+                <a href="tel:+27605405568">
+                    <img src="images/telephone.png" style="max-width: 20px; border-radius: 15px;">
+                    +27 60 540 5568
+                </a>
+            </p>
+
+            <p>
+                <a href="mailto:rohven21@gmail.com">
+                    <img src="images/email.png" style="max-width: 20px;">
+                    rohven21@gmail.com
+                </a>
+            </p>
+        </div>
     `;
     setActiveLink(document.getElementById("contact-link"));
 }
@@ -93,7 +216,12 @@ document.addEventListener("DOMContentLoaded", () => {
         e.preventDefault();
         loadEducation();
     });
-
+    
+    document.getElementById("project-link").addEventListener("click", (e) => {
+        e.preventDefault();
+        loadProject();
+    });
+    
     document.getElementById("contact-link").addEventListener("click", (e) => {
         e.preventDefault();
         loadContact();
